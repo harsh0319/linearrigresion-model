8c61ade39f9d8e68366909c007423c7ce2f487c7
harsh0319 <78340872+harsh0319@users.noreply.github.com>
2022-07-28 15:08:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 14:31:39 2022
+
+@author: Harsh Bansal
+"""
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+#from flask_ngrok import run_with_ngrok
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('linearregression.pkl','rb')) 
+#run_with_ngrok(app)
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    exp = float(request.args.get('exp'))
+    
+    prediction = model.predict([[exp]])
+    
+        
+    return render_template('index.html', prediction_text='Regression Model  has predicted salary for given experinace is : {}'.format(prediction))
+
+
+app.run(debug=True)
